4e80fd5d8a1620269c117f2b45f180468174c963
filipkustoch <filip.kustoch1@gmail.com>
2023-03-11 17:16:56 +0100

    Użycie scss zamiast css

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.scss">
+    <link rel="stylesheet" href="style1.css">
 </head>
 
 <body>
